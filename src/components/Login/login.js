1b035fe9c5eb52e4b8d14fd25d2a4f35c12f742d
chenlang1314 <lang_strive@163.com>
2018-04-16 18:16:45 +0800

    7

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './login.css';
 import '../../base.css';
-import { Input ,Button } from 'antd';
+import { Input ,Button ,Modal,message } from 'antd';
 import 'antd/dist/antd.css';
 import {withRouter} from 'react-router-dom';
 
@@ -9,9 +9,36 @@ import {withRouter} from 'react-router-dom';
 class Login extends Component{
     constructor(props){
         super(props);
+        this.state={
+            visible: false,
+            modal:''
+        }
+    }
+    componentDidMount(){
+        console.log(document.querySelector('.ant-modal'))
     }
     handleClick_login(){
         this.props.history.push('/home');
+    }
+    handleClick_showModal(){
+        this.setState({
+            visible: true
+        })
+    }
+    handleOk(){
+        if(this.state.modal==''){
+            message.info('大哥，请输入秘钥！');
+        }
+    }
+    handleChange_modal(e){
+        this.setState({
+            modal:e.target.value
+        })
+    }
+    handleCancel(){
+        this.setState({
+            visible: false
+        })
     }
     render(){
         return(
@@ -24,6 +51,20 @@ class Login extends Component{
                     <p><i>*</i><span>密码</span></p>   
                     <Input placeholder="请输入密码" maxLength="18" type="password" className="login_input"/>
                     <Button type="primary" onClick={this.handleClick_login.bind(this)} className="login_primary">登陆</Button>
+                    <strong className="reg" onClick={this.handleClick_showModal.bind(this)}>注册账号</strong>  
+                    <Modal
+                        title="友情提示"
+                        visible={this.state.visible}
+                        onOk={this.handleOk.bind(this)}
+                        onCancel={this.handleCancel.bind(this)}
+                        className="modal"
+                        ref="Modal"
+                        okText="确定"
+                        cancelText="取消"
+                        >
+                        <p style={{color:'#48576a',fontSize: '14px'}}>请输入邀请码</p>
+                        <p><Input placeholder="请输入账号" maxLength="120" style={{height:'35px'}} onChange={this.handleChange_modal.bind(this)}/></p>
+                    </Modal>
                 </div> 
             </div>
         )
