6e74a73e2f03c3eed7647fce89a697117148cb0a
chenlang1314 <lang_strive@163.com>
2018-04-17 18:04:47 +0800

    88

--- node/server.js
+++ node/server.js
@@ -1,15 +1,114 @@
 const express = require('express');
-var server=express();
-var RouterUser=express.Router();
-server.listen(8088);
+const fs = require('fs');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const expressStatic = require('express-static');
 
+
+var server=express();
+
+server.listen(8088,'localhost');
+
+/*数据库连接池*/
+var db=mysql.createPool({
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'123456',
+    database:'strive'
+});
+
+/*body-parser*/
+server.use(bodyParser.urlencoded({
+    extended:false
+}));
+
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials','true');
+    next();
+};
+server.use(allowCrossDomain);
+
+
+/*设置路由*/
+var RouterUser=express.Router();
 server.use('/',RouterUser);
+
 /*登陆*/
-RouterUser.get('/login',(req,res)=>{
-    res.send('aaa');
+RouterUser.post('/login',(req,res)=>{
+    let json=req.body;
+    console.log(json)
+    if(!json.username){
+        res.send({"number":"-1","msg":"用户名为空！"});
+    }else if(!json.password){
+        res.send({"number":"-1","msg":"密码名为空！"});
+    }else{
+        let param = [json.username];
+        var statement = 'SELECT * FROM login WHERE username= ?';
+        db.query(statement,param,(err,data)=>{
+            if(err){
+                res.status(500).send({number:"-1",msg:'注册失败！'});
+            }else{
+                if(!data.length){
+                    res.send({number:"-1",msg:'没有此用户！'});
+                }else{
+                    let param2 = [json.username,json.password];
+                    var statement2 = 'SELECT * FROM login WHERE username= ? AND password= ?';
+                    db.query(statement2,param2,(err,data)=>{
+                        if(err){
+                            res.status(500).send({number:"-1",msg:'注册失败！'});
+                        }else{
+                            if(!data.length){
+                                res.send({number:"-2",msg:'密码错误！'});
+                            }else{
+                                res.send({number:"0",msg:'登陆成功！'});
+                            }
+                        }
+                    })
+                }
+            }
+        })
+    }
 });
 
 /*注册*/
-RouterUser.get('/reg',(req,res)=>{
-    res.send('bbb');
+RouterUser.post('/reg',(req,res)=>{
+    let json=req.body;
+    console.log(json)
+    if(!json.username){
+        res.send({"number":"-1","msg":"用户名为空！"});
+    }else if(!json.password){
+        res.send({"number":"-1","msg":"密码名为空！"});
+    }else if(!json.name){
+        res.send({"number":"-1","msg":"真实姓名为空！"});
+    }else{
+        let param = [json.username];
+        var statement = 'SELECT * FROM login WHERE username= ?';
+        db.query(statement,param,(err,data)=>{
+            if(err){
+                console.log(json.username)
+                console.log(err)
+                res.status(500).send({number:"-1",msg:'注册失败！'});
+            }else{
+                if(!data.length){
+                    let param2=[json.username,json.password,json.name];
+                    let statement2='INSERT INTO login (username,password,name) VALUES( ?, ?, ?)'
+                    db.query(statement2,param2,(err,data)=>{
+                        if(err){
+                            res.status(500).send({number:"-1",msg:'注册失败！'});
+                        }else{
+                            console.log(data);
+                            res.send({"number":"0","msg":"注册成功！"});
+                        }
+                    })
+                }else{
+                    res.send({number:"-1",msg:'此用户已注册！'});
+                }
+            }
+        })
+    }
 });
--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -4,6 +4,7 @@ import '../../base.css';
 import { Input ,Button ,Modal,message } from 'antd';
 import 'antd/dist/antd.css';
 import {withRouter} from 'react-router-dom';
+import axios from 'axios';
 
 
 class Login extends Component{
@@ -14,14 +15,46 @@ class Login extends Component{
             modal:'',
             secret_key:'6ebe76c9fb411be97b3b0d48b791a7c9',
             login_box:true,
-            reg_box:false
+            reg_box:false,
+            reg_username:'',
+            reg_password:'',
+            reg_name:'',
+            login_username:'',
+            login_password:''
         }
     }
     componentDidMount(){
-
+        
     }
     handleClick_login(){
-        this.props.history.push('/home');
+        if(!this.state.login_username&&!this.state.login_password){
+            message.info('账号，密码为空！');
+        }else if(!this.state.login_username){
+            message.info('账号为空！');
+        }else if(!this.state.login_password){
+            message.info('密码为空！');
+        }else{
+            let params = new URLSearchParams();
+            params.append('username',this.state.login_username);  
+            params.append('password',this.state.login_password);
+            axios({
+                url:'http://localhost:8088/login',
+                data:params,
+                method: 'post'
+            }).then(data=>{
+                console.log(data)
+                if(data.data.number=='-1'){
+                    message.info(data.data.msg);
+                }else if(data.data.number=='0'){
+                    message.success(data.data.msg);
+                    this.props.history.push('/home');
+                }else if(data.data.number=='-2'){
+                    message.error(data.data.msg);
+                }
+            }).catch((res)=>{
+                message.error('服务器错误！');
+            })
+        }
     }
     handleClick_showModal(){
         this.setState({
@@ -29,7 +62,7 @@ class Login extends Component{
         })
     }
     handleOk(){
-        if(this.state.modal==''){
+        if(!this.state.modal){
             message.info('请输入秘钥！');
         }else if(this.state.modal!=this.state.secret_key){
             message.error('秘钥错误！');
@@ -52,6 +85,55 @@ class Login extends Component{
             visible:false
         })
     }
+    handleChange_reg(name,e){
+        let newState={};
+        newState[name]=e.target.value
+        this.setState(newState)
+    }
+    handleChange_login(name,e){
+        let newState={};
+        newState[name]=e.target.value;
+        this.setState(newState)
+    }
+    handleClick_reg(){
+        if(!this.state.reg_name&&!this.state.reg_username&&!this.state.reg_password){
+            message.info('真实姓名，用户名，密码都为空！');
+        }else if(!this.state.reg_name){
+            message.info('真实姓名为空！');
+        }else if(!this.state.reg_username){
+            message.info('用户名为空！');
+        }else if(!this.state.reg_password){
+            message.info('密码为空！');
+        }else{
+            let params = new URLSearchParams();
+            params.append('username',this.state.reg_username);  
+            params.append('password',this.state.reg_password);
+            params.append('name',this.state.reg_name);
+            axios({
+                url:'http://localhost:8088/reg',
+                data:params,
+                method: 'post'
+            }).then(data=>{
+                console.log(data)
+                if(data.data.number=='-1'){
+                    message.info(data.data.msg);
+                    this.setState({
+                        login_box:true,
+                        reg_box:false  
+                    }) 
+                }else if(data.data.number=='0'){
+                    message.success(data.data.msg);
+                    this.setState({
+                        login_box:true,
+                        reg_box:false  
+                    }) 
+                }
+            }).catch((res)=>{
+                message.error('服务器错误！');
+            })
+
+        }
+    }
     render(){
         return(
             <div className="login_banner">
@@ -59,9 +141,9 @@ class Login extends Component{
                 <p className="login_describe">这是一个自己买服务器，配置，写前后台代码的网站。</p>
                 <div className="login_modal_box" style={{display:this.state.login_box==true?'block':'none'}}>
                     <p><i>*</i><span>账号</span></p>
-                    <Input placeholder="请输入账号" maxLength="11" className="login_input"/>
+                    <Input placeholder="请输入账号" maxLength="11" className="login_input" onChange={this.handleChange_login.bind(this,'login_username')}/>
                     <p><i>*</i><span>密码</span></p>   
-                    <Input placeholder="请输入密码" maxLength="18" type="password" className="login_input"/>
+                    <Input placeholder="请输入密码" maxLength="18" type="password" className="login_input" onChange={this.handleChange_login.bind(this,'login_password')}/>
                     <Button type="primary" onClick={this.handleClick_login.bind(this)} className="login_primary">登陆</Button>
                     <strong className="reg" onClick={this.handleClick_showModal.bind(this)}>注册账号</strong>  
                     <Modal
@@ -80,12 +162,12 @@ class Login extends Component{
                 </div> 
                 <div className="login_modal_box_reg" style={{display:this.state.reg_box==true?'block':'none'}}>
                     <p><i>*</i><span>真实姓名</span></p>
-                    <Input placeholder="请输入姓名" maxLength="4" className="login_input"/>
+                    <Input placeholder="请输入姓名" maxLength="4" className="login_input" onChange={this.handleChange_reg.bind(this,'reg_name')}/>
                     <p><i>*</i><span>用户名</span></p>
-                    <Input placeholder="请输入账号" maxLength="11" className="login_input"/>
+                    <Input placeholder="请输入账号" maxLength="11" className="login_input" onChange={this.handleChange_reg.bind(this,'reg_username')}/>
                     <p><i>*</i><span>密码</span></p>
-                    <Input placeholder="请输入密码" maxLength="18" type="password" className="login_input"/>
-                    <Button type="primary" className="login_primary">注册</Button>
+                    <Input placeholder="请输入密码" maxLength="18" type="password" className="login_input" onChange={this.handleChange_reg.bind(this,'reg_password')}/>
+                    <Button type="primary" className="login_primary" onClick={this.handleClick_reg.bind(this)}>注册</Button>
                 </div>
             </div>
         )
